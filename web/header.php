219547d7c2c17afd2fcc53f752d19c5304ffdc33
zradford <rad12009@byui.edu>
2019-01-17 11:23:14 -0700

    updates to homepage

---
+++ web/header.php
@@ -0,0 +1,5 @@
+<ul>
+   <li><a href="home.php">Home</a></li>
+   <li><a href="contact.php">Contact</li>
+   <li><a href="homework.php">Classwork</li>
+</ul>
---
+++ web/home.php
@@ -0,0 +1,21 @@
+<!DOCTYPE <!DOCTYPE html>
+<html>
+<head>
+   <title>Zac Radford</title>
+   <link rel="stylesheet" type="text/css" href="index.css">
+</head>
+<body>
+   <?php include "header.php"?>
+   <div class="homeWrapper">
+      <div class="bigHello">
+         HI
+      </div>
+      <div class="introduction">
+         My name is Zac Radford, Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur laudantium voluptatum distinctio natus reprehenderit iusto voluptate neque omnis. Suscipit illo iste consectetur porro aperiam architecto. Eum beatae reiciendis harum dignissimos?
+      </div>
+      <div class="moreInfo">
+         Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad libero officiis quis beatae harum rerum incidunt ipsa optio asperiores! Ea eos voluptatem ipsum officia. Nostrum fugiat veritatis a accusantium rem!
+      </div>
+   </div>
+</body>
+</html>
