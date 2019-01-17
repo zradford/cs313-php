f7e524bf4d49bc614f99e3e93eadc8e98681c3c7
zradford <rad12009@byui.edu>
2019-01-17 11:33:57 -0700

    updates to homepage

--- web/header.php
+++ web/header.php
@@ -1,5 +1,5 @@
 <ul>
    <li><a href="home.php">Home</a></li>
-   <li><a href="contact.php">Contact</li>
-   <li><a href="homework.php">Classwork</li>
+   <li><a href="contact.php">Contact</a></li>
+   <li><a href="homework.php">Classwork</a></li>
 </ul>
