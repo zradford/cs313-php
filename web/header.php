5c25813ee9f541cbf5c7d8a7cb45ca969f9baa9e
zradford <rad12009@byui.edu>
2019-01-19 22:02:00 -0700

    more changes to fix the navbar

--- web/header.php
+++ web/header.php
@@ -5,13 +5,13 @@ if(basename(__FILE__) == "home.php"){
    <li><a href=\"contact.php\">Contact</a></li>
    <li><a href=\"homework.php\">Classwork</a></li>
    </ul>";
-} elseif (basename(__file__) == "contact.php") {
+} elseif (basename(__FILE__) == "contact.php") {
     echo "<ul>
     <li><a href=\"home.php\">Home</a></li>
    <li><a href=\"contact.php\" style=\"background-color:#C1C1C1\">Contact</a></li>
    <li><a href=\"homework.php\">Classwork</a></li>
    </ul>";
-} elseif (basename(__file__) == "homework.php"){
+} elseif (basename(__FILE__) == "homework.php"){
     echo "<ul><li><a href=\"home.php\">Home</a></li>
    <li><a href=\"contact.php\">Contact</a></li>
    <li><a href=\"homework.php\" style=\"background-color:#C1C1C1\">Classwork</a></li></ul>";
--- web/home.php
+++ web/home.php
@@ -2,12 +2,13 @@
 <html>
 <head>
    <title>Zac Radford</title>
-   <link href="index.css" rel="stylesheet" type="text/css">
+  
    <link href="https://fonts.googleapis.com/css?family=Fjalla+One" rel="stylesheet">
    <link href="https://fonts.googleapis.com/css?family=Rakkas" rel="stylesheet">
+   <link href="index.css" rel="stylesheet" type="text/css">
 </head>
 <body>
-    <?php echo file_get_contents(“header.php”); ?> 
+    <?php require("header.php") ?> 
    <div class="homeWrapper">
       <div class="bigHello">
          Hello
