507155aed4fccf8b07f6d3b1048a94e4369299fe
zradford <rad12009@byui.edu>
2019-01-19 21:17:49 -0700

    changes to header.php

--- web/header.php
+++ web/header.php
@@ -1,5 +1,21 @@
-<ul>
-   <li><a href="home.php">Home</a></li>
-   <li><a href="contact.php">Contact</a></li>
-   <li><a href="homework.php">Classwork</a></li>
-</ul>
+<?php
+if(basename(__FILE__) == "home.php"){
+   echo "<ul>
+   <li><a href=\"home.php\" style=\"background-color:#C1C1C1\">Home</a></li>
+   <li><a href=\"contact.php\">Contact</a></li>
+   <li><a href=\"homework.php\">Classwork</a></li>
+   </ul>";
+} elseif (basename(__file__) == "contact.php") {
+    echo "<ul>
+    <li><a href=\"home.php\">Home</a></li>
+   <li><a href=\"contact.php\" style=\"background-color:#C1C1C1\">Contact</a></li>
+   <li><a href=\"homework.php\">Classwork</a></li>
+   </ul>";
+} elseif (basename(__file__) == "homework.php"){
+    echo "<ul><li><a href=\"home.php\">Home</a></li>
+   <li><a href=\"contact.php\">Contact</a></li>
+   <li><a href=\"homework.php\" style=\"background-color:#C1C1C1\">Classwork</a></li></ul>";
+}
+
+
+?>
