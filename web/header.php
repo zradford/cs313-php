f7e0c5b3fb331a216ffc09595dc8c4a90bb09edd
zradford <rad12009@byui.edu>
2019-01-24 10:32:09 -0700

    navbar debug

--- web/header.php
+++ web/header.php
@@ -5,8 +5,8 @@ $page = basename(server(PHP_SELF));
 ?>
 
 <ul>
-   <li><a class="<?php if($page == "home.php") {echo "active"}?>" href="home.php">Home</a></li>
-   <li><a class="<?php if($page == "contact.php") {echo "active"}?>" href="contact.php">Contact</a></li>
-   <li><a class="<?php if($page == "homework.php") {echo "active"}?>" href="homework.php">Classwork</a></li>
+   <li><a class="<?php if($page == "home.php") {echo "active";}?>" href="home.php">Home</a></li>
+   <li><a class="<?php if($page == "contact.php") {echo "active";}?>" href="contact.php">Contact</a></li>
+   <li><a class="<?php if($page == "homework.php") {echo "active";}?>" href="homework.php">Classwork</a></li>
 </ul>
 
