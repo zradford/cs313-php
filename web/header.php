d267be50ce028644f888ed4697dd7e60770b128d
zradford <rad12009@byui.edu>
2019-01-24 11:14:14 -0700

    style changes, update to homework.php

--- web/header.php
+++ web/header.php
@@ -2,7 +2,7 @@
 $page = basename($_SERVER[PHP_SELF]);
 ?>
 
-<ul>
+<ul class="rakkas">
    <li><a class="<?php if($page == "home.php") {echo "active";}?>" href="home.php">Home</a></li>
    <li><a class="<?php if($page == "contact.php") {echo "active";}?>" href="contact.php">Contact</a></li>
    <li><a class="<?php if($page == "homework.php") {echo "active";}?>" href="homework.php">Classwork</a></li>
