7975302b3b9f5399493adad32c71430a56cbf3c8
zradford <rad12009@byui.edu>
2019-01-24 10:28:22 -0700

    navbar fixes

--- web/header.php
+++ web/header.php
@@ -1,28 +1,12 @@
-<?php
-$path_parts = pathinfo()
-echo $path_parts['dirname'], "\n";
-echo $path_parts['basename'], "\n";
-echo $path_parts['extension'], "\n";
-echo $path_parts['filename'], "\n";
-// if(__FILE__ == "home.php"){
-//    echo "<ul>
-//    <li><a href=\"home.php\" style=\"background-color:#C1C1C1\">Home</a></li>
-//    <li><a href=\"contact.php\">Contact</a></li>
-//    <li><a href=\"homework.php\">Classwork</a></li>
-//    </ul>";
-// } elseif (__FILE__ == "contact.php") {
-//     echo "<ul>
-//     <li><a href=\"home.php\">Home</a></li>
-//    <li><a href=\"contact.php\" style=\"background-color:#C1C1C1\">Contact</a></li>
-//    <li><a href=\"homework.php\">Classwork</a></li>
-//    </ul>";
-// } elseif (__FILE__ == "homework.php"){
-//     echo "<ul><li><a href=\"home.php\">Home</a></li>
-//    <li><a href=\"contact.php\">Contact</a></li>
-//    <li><a href=\"homework.php\" style=\"background-color:#C1C1C1\">Classwork</a></li></ul>";
-// }
+<!DOCTYPE html>
 
-// ?>
-<!-- // <ul><li><a href="home.php">Home</a></li>
-//    <li><a href="contact.php">Contact</a></li>
-//    <li><a href="homework.php">Classwork</a></li></ul> -->
+<?php
+$page = basename(server(PHP_SELF));
+?>
+
+<ul>
+   <li><a class="<?php if($page == "home.php") {echo "active"}?>" href="home.php">Home</a></li>
+   <li><a class="<?php if($page == "contact.php") {echo "active"}?>" href="contact.php">Contact</a></li>
+   <li><a class="<?php if($page == "homework.php") {echo "active"}?>" href="homework.php">Classwork</a></li>
+</ul>
+
