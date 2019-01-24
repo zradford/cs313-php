1c0ce6e8fc0737df61bff2051110397dedb5697e
zradford <rad12009@byui.edu>
2019-01-24 10:41:58 -0700

    add header.php to all pages

--- web/homework.php
+++ web/homework.php
@@ -1,1 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+   <title>Zac Radford</title>
+  
+   <link href="https://fonts.googleapis.com/css?family=Fjalla+One" rel="stylesheet">
+   <link href="https://fonts.googleapis.com/css?family=Rakkas" rel="stylesheet">
+   <link href="index.css" rel="stylesheet" type="text/css">
+</head>
 <?php include 'header.php' ?>
