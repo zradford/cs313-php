01aefd0e521cb715e0a0fd8473d2fc078d989ee6
zradford <rad12009@byui.edu>
2019-01-26 11:46:01 -0700

    changed pictures to working ones

--- web/shoppingCart/cart.php
+++ web/shoppingCart/cart.php
@@ -1,3 +1,6 @@
+<?php 
+   session_start();
+?>
 <?php include "storeHeader.php";?>
 <!DOCTYPE html>
 <html>
--- web/shoppingCart/store.php
+++ web/shoppingCart/store.php
@@ -1,3 +1,6 @@
+<?php 
+   session_start();
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -16,27 +19,7 @@
          <div> 
             <h1>Kirby</h1>
             <p>$1,000</p>
-         </div>
-      </div>
-      <div class="storeItem card">
-         <img src="/pictures/mario.png" alt="Mario">
-         <div> 
-            <h1>Mario</h1>
-            <p>$5</p>
-         </div>
-      </div>
-      <div class="storeItem card">
-         <img src="/pictures/pichu.png" alt="pichu">
-         <div> 
-            <h1>Pichu</h1>
-            <p>$5</p>
-         </div>
-      </div>
-      <div class="storeItem card">
-         <img src="/pictures/shizue.png" alt="shizue">
-         <div> 
-            <h1>Isabelle</h1>
-            <p>$5</p>
+            <button></button> 
          </div>
       </div>
    </div>
