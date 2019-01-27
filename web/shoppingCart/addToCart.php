616c116226184cbc9009d8b4ebd68a3244b3c47b
zradford <rad12009@byui.edu>
2019-01-26 17:50:56 -0700

    addtocart.php debug

--- web/shoppingCart/addToCart.php
+++ web/shoppingCart/addToCart.php
@@ -10,13 +10,13 @@
    // if they already have that item, do nothing
    // else add it to the cart
    if( in_array($_REQUEST["item"], $cartItems){
-      ;
+      
    } else {
       $cartItems[] = $_REQUEST["item"];
       $_SESSION["numCartItems"] += 1;
    }
 
    // return the number of items in the cart.
-   echo $_SESSION["numCartItems"]; 
+   echo $_SESSION["numCartItems"];
 
 ?>
