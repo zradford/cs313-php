a810bbaf010ff93f4b6b41dc477fa0fc564eda35
zradford <rad12009@byui.edu>
2019-01-26 17:45:00 -0700

    addtocart updates for php and js

--- web/shoppingCart/storeHeader.php
+++ web/shoppingCart/storeHeader.php
@@ -1,12 +1,12 @@
 <?php
-$page = basename($_SERVER[PHP_SELF]);
+$page = basename($_SERVER['PHP_SELF']);
 ?>
 
 <ul class="rakkas">
    <li><a class="<?php if($page == "store.php") {echo "active";}?>" href="store.php">Home </a></li>
    <!-- <li><a class="<?php if($page == "") {echo "active";}?>" href="contact.php">   </a></li>
    <li><a class="<?php if($page == "") {echo "active";}?>" href="">   </a></li> -->
-   <li><a class="right <?php if($page == "cart.php") {echo "active";}?>" href="cart.php">Cart</a></li>
+   <li class="right"> <a class="" <?php if($page == "cart.php") {echo "active";}?>" href="cart.php">Cart</a></li>
 </ul>
 <div style="float: right; display:inline-block;" id="cartNum">
 0 <a href="cart.php"> <img src="" alt="cart"> </a>
