0c929e6e182242df9b8753d74f621817cdb3b528
zradford <rad12009@byui.edu>
2019-02-14 11:24:24 -0700

    debug

--- web/notes.php
+++ web/notes.php
@@ -7,7 +7,7 @@
    #get course from the db
    #:id is a place holder
    $query = 'SELECT course_id, course_name, course_code FROM course WHERE course_id = :id';
-   $statement = $db->query($query);
+   $statement = $db->prepare($query);
    #bindValue will change :id to what you want and make sure it's the right thing, 
    #protect against sql injection
    $statement->bindValue(':id', $course_id, PDO::PARAM_INT);
@@ -37,5 +37,10 @@ echo "<h1>Notes for $course_code - $course_name</h1>";
     <input type="hidden" name="course_id" value="<?php echo $course_id ?>">
     <textarea name="content"></textarea><br />
     <input type="submit" value="Insert Note">
+</form>
+
+<?php 
+
+?>
 </body>
 </html>
